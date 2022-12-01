a2fe93c5c5ba28b335243f7bf6eaa45af2ed5db0
ZawYeWint <zawyewint@my.jcu.edu.au>
2022-12-01 16:45:17 +0800

    Exercises

---
+++ CH-3/Percentages.java
@@ -0,0 +1,13 @@
+public class Percentages
+{
+   public static void main (String args[])
+   {
+      double num1 = 2.0, num2 = 5.0;
+      computePercent(num1, num2);
+      computePercent(num2, num1);
+   }
+   public static void computePercent(double x, double y)
+   {
+       System.out.println(x + " is " + (x / y) * 100 + " percent of " + y);
+   }
+}
